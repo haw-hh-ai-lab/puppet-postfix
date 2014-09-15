5e3ceb38ec470ee96861ad85c1638c352502c074
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2014-09-15 11:23:01 +0200

    add a test for new puppet class postfix::rule

---
+++ spec/classes/postfix_rule_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'postfix::rule', :type => :class do
+  let(:facts) { {:operatingsystem => 'Debian', :operatingsystemrelease => '7.1'} }
+  let(:params) { {
+    :name          => 'a_new_rule',
+    :rule          => 'the_data_to_set'
+  } }
+  it 'should have an augeas resource' do
+    should contain_augeas('postfix main.cf a_new_rule')
+  end
+  describe_augeas 'postfix main.cf a_new_rule', :lens => 'postfix_main', :target => 'etc/postfix/main.cf' do
+    it 'a_new_rule should be set to the_data_to_set' do
+      should execute.with_change
+
+      aug_get('a_new_rule').should == 'the_data_to_set' 
+
+      should execute.idempotently
+    end
+  end
+end
+
